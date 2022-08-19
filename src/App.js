19a84b05896649c188d265aa26adcca4a0daec53
KIRILL F <tiltlessman@gmail.com>
2022-08-19 11:58:09 +0300

    loading spinner created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SearchForm from './components/SearchForm'
 import BooksList from './features/books/BooksList'
+import LoadingSpinner from './components/LoadingSpinner'
 
 function App() {
   return (
     <>
       <SearchForm />
       <BooksList />
+      <LoadingSpinner />
     </>
   )
 }
